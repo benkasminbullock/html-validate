ddb2a9c744451481804f19eda997d287a5cfa61d
Ben Bullock <benkasminbullock@gmail.com>
2017-11-09 09:48:04 +0900

    Add to stringpos routine

--- validate.go
+++ validate.go
@@ -1,12 +1,12 @@
-package main;
+package main
 
 import (
-	"fmt"
-	"strings"
 	"errors"
-	"unicode/utf8"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
 
 var valid map[string]bool
@@ -14,12 +14,12 @@ var noClose map[string]bool
 var nonNesting map[string]bool
 
 type lineTag struct {
-	tag string
+	tag  string
 	line int
 }
 
 func makeMap(list []string) (z map[string]bool) {
-	z = make(map [string]bool)
+	z = make(map[string]bool)
 	for _, tag := range list {
 		z[tag] = true
 	}
@@ -42,8 +42,8 @@ func initValid() {
 
 type stringPos struct {
 	position int
-	i int
-	line int
+	i        int
+	line     int
 	filename string
 	nletters int
 }
@@ -51,23 +51,27 @@ type stringPos struct {
 func (sp *stringPos) String() string {
 	return fmt.Sprintf("%s:%d:", sp.filename, sp.line)
 }
+func (sp *stringPos) Add(c string) {
+	sp.i++
+	sp.position += len(c)
+}
 
 // Jump over jumpbytes bytes, adjusting the strings and counting
 // newlines.
 
-func jumpover(html string, jumpbytes int, sp * stringPos) {
-	jump := html[(*sp).position:(*sp).position+jumpbytes]
+func jumpover(html string, jumpbytes int, sp *stringPos) {
+	jump := html[(*sp).position : (*sp).position+jumpbytes]
 	addlines := strings.Count(jump, "\n")
-/*
-	fmt.Printf("Jumping over '%s'.\n", jump)
-	fmt.Printf("Adding %d lines.\n", addlines)
-*/
+	/*
+		fmt.Printf("Jumping over '%s'.\n", jump)
+		fmt.Printf("Adding %d lines.\n", addlines)
+	*/
 	sp.line += addlines
 	sp.i += utf8.RuneCountInString(jump)
 	sp.position += jumpbytes
 }
 
-func findIds(ids map[string]lineTag, tag string, remaining string, sp * stringPos) {
+func findIds(ids map[string]lineTag, tag string, remaining string, sp *stringPos) {
 	// The located ID.
 	var id string
 	idequal := strings.Index(remaining, "id=")
@@ -76,13 +80,13 @@ func findIds(ids map[string]lineTag, tag string, remaining string, sp * stringPo
 		if openquote != -1 {
 			closequote := strings.IndexAny(remaining[idequal+openquote+2:], "\"'")
 			if closequote != -1 {
-				id = remaining[idequal+openquote+2:idequal+openquote+2+closequote]
-//				fmt.Printf("%s found id '%s'\n", sp, id);
+				id = remaining[idequal+openquote+2 : idequal+openquote+2+closequote]
+				//				fmt.Printf("%s found id '%s'\n", sp, id);
 				if lt, exists := ids[id]; exists {
 					fmt.Printf("%s:%d: duplicate id '%s'\n",
-						sp.filename, sp.line, id);
+						sp.filename, sp.line, id)
 					fmt.Printf("%s:%d: previous instance in tag '%s'\n",
-						sp.filename, lt.line, lt.tag) 
+						sp.filename, lt.line, lt.tag)
 				} else {
 					var lt lineTag
 					lt.line = sp.line
@@ -94,12 +98,12 @@ func findIds(ids map[string]lineTag, tag string, remaining string, sp * stringPo
 	}
 }
 
-func findTag(html string, sp * stringPos, open bool, ids map[string]lineTag) (tag string, err error) {
+func findTag(html string, sp *stringPos, open bool, ids map[string]lineTag) (tag string, err error) {
 	start := strings.IndexAny(html[sp.position:], " >\n")
 	if start == -1 {
 		return "", errors.New("No ending marker found")
 	}
-	tag = html[(*sp).position:(*sp).position+start]
+	tag = html[(*sp).position : (*sp).position+start]
 	end := strings.IndexAny(html[(*sp).position:], ">")
 	if end == -1 {
 		return "", errors.New("No > after <")
@@ -107,50 +111,50 @@ func findTag(html string, sp * stringPos, open bool, ids map[string]lineTag) (ta
 
 	// ids as nil is used for end tags.
 	if open && start+1 < end {
-		remaining := html[sp.position+start+1:sp.position+end]
+		remaining := html[sp.position+start+1 : sp.position+end]
 		if len(remaining) > 0 {
 			findIds(ids, tag, remaining, sp)
 		}
 	}
 	jumpover(html, end, sp)
- 	return tag, err
+	return tag, err
 }
 
 /* Skip over HTML comments or doctype declarations. */
 
-func skipComment(html string, sp * stringPos) (err error) {
+func skipComment(html string, sp *stringPos) (err error) {
 	var end int
 	start := html[sp.position:]
 	if strings.ToLower(start[0:7]) == "doctype" {
 		end = strings.Index(start, ">")
-		if (end == -1) {
+		if end == -1 {
 			return errors.New("No > found for doctype")
 		}
 	} else if start[0:2] == "--" {
 		end = strings.Index(start, "-->")
-		if (end == -1) {
+		if end == -1 {
 			return errors.New("No --> found for comment")
 		}
 	} else {
 		return errors.New("<! but no doctype or comment marker")
 	}
-	jumpover (html, end, sp)
+	jumpover(html, end, sp)
 	return
 }
 
 /* Skip over <script>..</script>. */
 
-func skipScript(html string, sp * stringPos) (err error) {
+func skipScript(html string, sp *stringPos) (err error) {
 	var end int
 	start := html[sp.position:]
 	end = strings.Index(start, "</script>")
-	if (end == -1) {
-		fmt.Printf("%s no </script> found.\n", sp);
+	if end == -1 {
+		fmt.Printf("%s no </script> found.\n", sp)
 		return errors.New("No </script> found for <script>")
 	}
 	// This leaves the </script> tag at the start of what sp points
 	// to, then it is handled by the usual closing tag stuff.
-	jumpover (html, end, sp)
+	jumpover(html, end, sp)
 	return
 }
 
@@ -171,8 +175,7 @@ func validate(html string, filename string, offset int) {
 		c := split[sp.i]
 		switch c {
 		case "<":
-			sp.i++
-			sp.position += len(c)
+			sp.Add(c)
 			c := split[sp.i]
 			switch c {
 			case "/":
@@ -180,22 +183,22 @@ func validate(html string, filename string, offset int) {
 				/* Closing tag, pop the stack "opentags" to find a
 				/* match. */
 
-				sp.i++
-				sp.position += len(c)
-				tag, err := findTag(html, & sp, false, ids)
+				sp.Add(c)
+
+				tag, err := findTag(html, &sp, false, ids)
 				if err != nil {
 					fmt.Printf("%s:%d: error %s\n",
 						filename, sp.line, err)
 				} else {
-					delete (nestTags, tag)
+					delete(nestTags, tag)
 					var toptag lineTag
 					if len(opentags) > 0 {
-						opentags, toptag = opentags[:len(opentags) - 1], opentags[len(opentags) - 1]
+						opentags, toptag = opentags[:len(opentags)-1], opentags[len(opentags)-1]
 					} else {
 						fmt.Printf("%s:%d: too many closing tags.\n",
-							filename, sp.line);
+							filename, sp.line)
 					}
-					if (toptag.tag != tag) {
+					if toptag.tag != tag {
 						closed := false
 						for scrape := len(opentags) - 1; scrape >= 0; scrape-- {
 							scrapeTag := opentags[scrape]
@@ -203,19 +206,19 @@ func validate(html string, filename string, offset int) {
 								fmt.Printf("%s:%d: tag mismatch: <%s> </%s>: ",
 									filename, sp.line, toptag.tag, tag)
 								fmt.Printf("popping %d unclosed tags.\n",
-									len(opentags) - scrape)
+									len(opentags)-scrape)
 								for i := scrape + 1; i < len(opentags); i++ {
 									fmt.Printf("%s:%d: <%s> unclosed.\n",
-										filename, opentags[i].line, opentags[i].tag);
+										filename, opentags[i].line, opentags[i].tag)
 								}
 								fmt.Printf("%s:%d: <%s> unclosed.\n",
-									filename, toptag.line, toptag.tag);
+									filename, toptag.line, toptag.tag)
 								opentags = opentags[:scrape]
 								closed = true
 								break
 							}
 						}
-						if ! closed {
+						if !closed {
 							fmt.Printf("%s:%d: closing tag </%s> with no opening tag.\n",
 								filename, sp.line, tag)
 							// Push the last thing back on there.
@@ -228,16 +231,15 @@ func validate(html string, filename string, offset int) {
 				fmt.Printf("%s:%d: space character after <.\n",
 					filename, sp.line)
 			case "!":
-				sp.i++
-				sp.position += len(c)
-				err := skipComment(html, & sp)
+				sp.Add(c)
+				err := skipComment(html, &sp)
 				if err != nil {
 					fmt.Printf("%s:%d: error %s\n",
 						filename, sp.line, err)
 				}
 			default:
-				tag, err := findTag(html, & sp, true, ids)
-				if ! valid[tag] {
+				tag, err := findTag(html, &sp, true, ids)
+				if !valid[tag] {
 					fmt.Printf("%s:%d: unknown tag <%s>.\n",
 						filename, sp.line, tag)
 				}
@@ -245,7 +247,7 @@ func validate(html string, filename string, offset int) {
 					fmt.Printf("%s:%d: error %s\n",
 						filename, sp.line, err)
 				} else {
-					if ! noClose[tag] {
+					if !noClose[tag] {
 						var lt lineTag
 						lt.tag = tag
 						lt.line = sp.line
@@ -254,25 +256,24 @@ func validate(html string, filename string, offset int) {
 							previous, nested := nestTags[tag]
 							if nested {
 								fmt.Printf("%s:%d: nested <%s>.\n",
-									filename, sp.line, tag);
+									filename, sp.line, tag)
 								fmt.Printf("%s:%d: first <%s> is here.\n",
 									filename, previous.line, tag)
 							} else {
-								nestTags[tag] = lt;
+								nestTags[tag] = lt
 							}
 						}
 					}
 				}
 				if tag == "script" {
-					skipScript(html, & sp)
+					skipScript(html, &sp)
 				}
 			}
 		case "\n":
-			sp.line++;
+			sp.line++
 			fallthrough
 		default:
-			sp.i++
-			sp.position += len(c)
+			sp.Add(c)
 		}
 	}
 	if len(opentags) > 0 {
@@ -283,23 +284,21 @@ func validate(html string, filename string, offset int) {
 	}
 }
 
-//http://stackoverflow.com/questions/13514184/how-can-i-read-a-whole-file-into-a-string-variable-in-golang#13514395
-
 func main() {
 	initValid()
 	for i := 1; i < len(os.Args); i++ {
+		//http://stackoverflow.com/questions/13514184/how-can-i-read-a-whole-file-into-a-string-variable-in-golang#13514395
 		file := os.Args[i]
 		buf, err := ioutil.ReadFile(file)
 		if err != nil {
-			fmt.Println("error file ",err)
-			continue;
+			fmt.Println("error file ", err)
+			continue
 		}
-		if ! utf8.Valid(buf) {
-			fmt.Printf("%s is not UTF-8.\n", file);
-			continue;
+		if !utf8.Valid(buf) {
+			fmt.Printf("%s is not UTF-8.\n", file)
+			continue
 		}
 		s := string(buf)
-		validate (s, file, 0)
+		validate(s, file, 0)
 	}
 }
-
